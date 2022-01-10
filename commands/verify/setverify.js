6e74f8bc7ab25f58315a0d625ab42c32703ecbbb
BedsRoom <91224083+BedsRoom@users.noreply.github.com>
2022-01-10 10:20:31 -0800

    Update setverify.js

--- commands/verify/setverify.js
+++ commands/verify/setverify.js
@@ -9,14 +9,14 @@ module.exports = {
 
   
      if (!message.member.hasPermission("ADMINISTRATION")) {
-      return message.channel.send("You do not enough permission to use this command.");
+      return message.channel.send("You do not enough permission to use this command. [Permission Needed-Administrator]");
     }
     let channel = message.mentions.channels.first()
     
     if(!channel) {
       return message.channel.send("You have to specify the channel")
     }
-   // This code is made by Supreme#2401
+   
     db.set(`verify_${message.guild.id}`, channel.id)
     
     
